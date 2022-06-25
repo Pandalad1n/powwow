feb697f9e442d51819640ffd464384448e6bb661
a.romantsev <a.romantsev@pangeoradar.ru>
2022-06-25 21:37:11 +0300

    move handler to rpc package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,52 +2,14 @@ package main
 
 import (
 	"context"
-	"github.com/Pandalad1n/powwow/hashcash"
-	pb "github.com/Pandalad1n/powwow/proto"
+	"github.com/Pandalad1n/powwow/cmd/server/rpc"
 	"github.com/Pandalad1n/powwow/tcp"
-	"github.com/Pandalad1n/powwow/wisdom"
-	"google.golang.org/protobuf/proto"
 	"log"
 )
 
 func main() {
 	ctx := context.Background()
-	err := tcp.ListenAndServe(ctx, "0.0.0.0:8888", tcp.HandleFunc(func(conn tcp.Conn) {
-		c := hashcash.NewChallenge(10)
-		cMessage := pb.Message{Payload: &pb.Message_Challenge{Challenge: &pb.Challenge{Digest: c.Digest, Difficulty: c.Difficulty}}}
-		pMessage, err := proto.Marshal(&cMessage)
-		if err != nil {
-			return
-		}
-		err = conn.WriteMessage(pMessage)
-		if err != nil {
-			return
-		}
-		resp, err := conn.ReadMessage()
-		if err != nil {
-			log.Fatalln(err)
-		}
-		pmsg := pb.Message{}
-		err = proto.Unmarshal(resp, &pmsg)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		switch pmsg.Payload.(type) {
-		case *pb.Message_Solution:
-			solution := pmsg.GetSolution()
-			if c.Verify(solution.Solution) {
-				wMessage := pb.Message{Payload: &pb.Message_Wisdom{Wisdom: &pb.Wisdom{Text: wisdom.GetWisdom()}}}
-				pMessage, err = proto.Marshal(&wMessage)
-				if err != nil {
-					return
-				}
-				err = conn.WriteMessage(pMessage)
-				if err != nil {
-					return
-				}
-			}
-		}
-	}))
+	err := tcp.ListenAndServe(ctx, "0.0.0.0:8888", rpc.Handler{})
 	if err != nil {
 		log.Fatalln(err)
 	}
--- cmd/server/rpc/rpc.go
+++ cmd/server/rpc/rpc.go
@@ -1,1 +1,50 @@
 package rpc
+
+import (
+	"github.com/Pandalad1n/powwow/hashcash"
+	pb "github.com/Pandalad1n/powwow/proto"
+	"github.com/Pandalad1n/powwow/tcp"
+	"github.com/Pandalad1n/powwow/wisdom"
+	"google.golang.org/protobuf/proto"
+	"log"
+)
+
+type Handler struct {
+}
+
+func (f Handler) ServeTCP(c tcp.Conn) {
+	challenge := hashcash.NewChallenge(10)
+	cMessage := pb.Message{Payload: &pb.Message_Challenge{Challenge: &pb.Challenge{Digest: challenge.Digest, Difficulty: challenge.Difficulty}}}
+	pMessage, err := proto.Marshal(&cMessage)
+	if err != nil {
+		return
+	}
+	err = c.WriteMessage(pMessage)
+	if err != nil {
+		return
+	}
+	resp, err := c.ReadMessage()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	pmsg := pb.Message{}
+	err = proto.Unmarshal(resp, &pmsg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	switch pmsg.Payload.(type) {
+	case *pb.Message_Solution:
+		solution := pmsg.GetSolution()
+		if challenge.Verify(solution.Solution) {
+			wMessage := pb.Message{Payload: &pb.Message_Wisdom{Wisdom: &pb.Wisdom{Text: wisdom.GetWisdom()}}}
+			pMessage, err = proto.Marshal(&wMessage)
+			if err != nil {
+				return
+			}
+			err = c.WriteMessage(pMessage)
+			if err != nil {
+				return
+			}
+		}
+	}
+}
