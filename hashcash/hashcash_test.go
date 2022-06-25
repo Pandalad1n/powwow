16218fac9a36275613b62600ae27e38e790e8d86
a.romantsev <a.romantsev@pangeoradar.ru>
2022-06-25 22:04:49 +0300

    add benchmark

--- hashcash/hashcash_test.go
+++ hashcash/hashcash_test.go
@@ -1,11 +1,29 @@
 package hashcash
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestChallenge(t *testing.T) {
 	c := NewChallenge(18)
 	solution := c.Solve()
 	if !c.Verify(solution) {
 		t.Fail()
+	}
+}
+
+func BenchmarkChallenge(b *testing.B) {
+	difficulties := [...]uint32{1, 5, 10, 15, 20}
+	for _, d := range difficulties {
+		b.Run(fmt.Sprintf("Testing %v difficulty", d), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				c := NewChallenge(d)
+				solution := c.Solve()
+				if !c.Verify(solution) {
+					b.Fail()
+				}
+			}
+		})
 	}
 }
